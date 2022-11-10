5a5f2c315c6f8d0569a19acdd7c6393b9b194ebc
kevin <67239585+kevinstif@users.noreply.github.com>
2022-11-09 20:32:41 -0500

    first version of portfolio

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,31 @@
 const navToggle = document.querySelector(".nav-toogle")
 const navMenu = document.querySelector(".nav-menu")
-const navMenuItem = document.querySelector(".nav-menu-item")
+const navMenuItem = document.querySelectorAll(".nav-menu-item")
+const MenuItem  = document.querySelectorAll('.menu-item')
+const skillIcon  = document.querySelectorAll('.skill-icon')
 
+const grande = document.querySelector('.grande')
+const punto  = document.querySelectorAll('.punto')
+const numProyects = 0;
+let operacion = 0
+
+//Header Navigation buttons hover effects
+navMenuItem.forEach( ( items , i )=> {
+    navMenuItem[i].addEventListener('click',()=>{
+
+        navMenuItem.forEach( ( items , i )=>{
+            navMenuItem[i].classList.remove('nav-menu-item-active')
+        })
+        navMenuItem[i].classList.add('nav-menu-item-active')
+        
+        navMenu.classList.toggle("nav-menu_visible")
+        if(navMenu.classList.contains("nav-menu_visible")){
+            navMenu.setAttribute("aria-label", "Cerrar menu")
+        }
+    })
+})
+
+//Header Navigation button hamburguer menu enable and disable
 navToggle.addEventListener("click",()=>{
     navMenu.classList.toggle("nav-menu_visible")
 
@@ -12,11 +36,21 @@ navToggle.addEventListener("click",()=>{
     }
 })
 
+//Skill icons hover
 
-const grande = document.querySelector('.grande')
-const punto  = document.querySelectorAll('.punto')
-const numProyects = 9;
-let operacion = 0
+skillIcon.forEach( ( items , i )=> {
+    skillIcon[i].addEventListener('mouseover',()=>{
+
+        skillIcon.forEach( ( items , i )=>{
+            skillIcon[i].classList.remove('colored')
+        })
+
+        skillIcon[i].classList.add('colored')
+        
+    })
+})
+
+//Move of Galery of proyects
 
 punto.forEach( ( cadaPunto , i )=> {
     punto[i].addEventListener('click',()=>{
@@ -39,4 +73,3 @@ punto.forEach( ( cadaPunto , i )=> {
 
     })
 })
-
